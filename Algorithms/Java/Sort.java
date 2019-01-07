f8399cefdf0e2ab2e78ee359848f7a63df8658d1
Tharwat <a.tharwat100@gmail.com>
2019-01-07 15:17:45 +0200

    Adding insertionSort

--- Algorithms/Java/Sort.java
+++ Algorithms/Java/Sort.java
@@ -14,4 +14,21 @@ public class Sort{
 			aArrayrr[Minimum] = temp;
 		}
 	}
+
+	// Time Complexity O(n^2)
+	public static void InsertionSort(int[] Array) {
+		int size = Array.length;
+		
+		for (int i = 0; i < size; i++) {
+			int ComparingValue = Array[i];
+			int j = i;
+			
+			while(j > 0 && ComparingValue < Array[j-1]) { // Comparing all left elements with the
+				Array[j] = Array[j-1];					  // Current one
+				j--;
+			}
+			
+			Array[j] = ComparingValue;					  // Reposition the element that used for comparison
+		}
+	}
 }
